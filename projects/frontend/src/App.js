c94f050bb3d7c307d0bcf866a3bfd1e0abad8325
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-06-13 19:26:51 +0700

    Add forgot-password page route

--- projects/frontend/src/App.js
+++ projects/frontend/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import LandingPage from "./pages/LandingPage";
+import { ForgotPasswordPage } from "./pages/ForgotPasswordPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage/>} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage/>} />
       </Routes>
     </Router>
   );
