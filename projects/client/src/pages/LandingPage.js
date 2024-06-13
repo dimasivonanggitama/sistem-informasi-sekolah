e76906d0cad512fd840ef894910fd527d174304b
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-06-13 21:09:29 +0700

    Added onClick props on Login Button

--- projects/client/src/pages/LandingPage.js
+++ projects/client/src/pages/LandingPage.js
@@ -1,14 +1,15 @@
 import { Button, Flex, HStack, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 
 const LandingPage = () => {
+  const navigate = useNavigate();
   return (
     <Flex minH={"100vh"} bg="grey">
       <VStack width={"100%"}>
         <HStack bg="orange" justifyContent={"space-between"} minW={"100vh"} padding={5}>
           <Text>Navigation Bar</Text>
-          <Button colorScheme='blue'>Login</Button>
+          <Button colorScheme='blue' onClick={() => navigate("loginPage")}>Login</Button>
         </HStack>
         <Text textColor={"white"}>This is LandingPage</Text>
       </VStack>
