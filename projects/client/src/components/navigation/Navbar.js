398bd6c88be79ddc3eb1fd5cffa750f831bfbcf2
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-06-14 21:19:50 +0700

    Set main logo as Button + navigate to landing page route

--- projects/client/src/components/navigation/Navbar.js
+++ projects/client/src/components/navigation/Navbar.js
@@ -1,11 +1,12 @@
-import { Box, HStack, Text } from '@chakra-ui/react'
+import { Button, HStack, Text } from '@chakra-ui/react'
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
-  return (
-    <HStack justifyContent={"flex-start"} bgColor={"lightgrey"} height={"18%"} width={"100%"} padding={5}>
-        <Box bgImage={"main.png"} bgPosition={"center"} bgRepeat={"no-repeat"} bgSize={"100%"} height={"100%"} width={"15%"} ></Box>
-        <Text>Navigation Bar</Text>
-    </HStack>
-  )
+     const navigate = useNavigate();
+    return (
+        <HStack justifyContent={"flex-start"} bgColor={"lightgrey"} height={"18%"} width={"100%"} padding={5}>
+            <Button bgImage={"main.png"} bgPosition={"center"} bgRepeat={"no-repeat"} bgSize={"100%"} height={"100%"} width={"15%"} onClick={() => navigate("/")} variant="link"></Button>
+        </HStack>
+    )
 }
