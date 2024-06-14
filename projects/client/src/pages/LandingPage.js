e4b4da9091d943f14b57541d1b41cd2d9695edd7
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-06-14 21:24:05 +0700

    Replaced some components to Navbar preset component

--- projects/client/src/pages/LandingPage.js
+++ projects/client/src/pages/LandingPage.js
@@ -1,15 +1,13 @@
 import { Button, Flex, HStack, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { Navbar } from '../components/navigation/Navbar'
 
 
 const LandingPage = () => {
   return (
-    <Flex minH={"100vh"} bg="grey">
+    <Flex minH={"100vh"} bg="pink">
       <VStack width={"100%"}>
-        <HStack bg="orange" justifyContent={"space-between"} minW={"100vh"} padding={5}>
-          <Text>Navigation Bar</Text>
-          <Button colorScheme='blue'>Login</Button>
-        </HStack>
+        <Navbar />
         <Text textColor={"white"}>This is LandingPage</Text>
       </VStack>
     </Flex>
