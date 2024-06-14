78140ac3f3d088e08f0fdb1c96dfcc151ffefd90
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-06-14 21:21:33 +0700

    Added border & shadow on Navbar

--- projects/client/src/components/navigation/Navbar.js
+++ projects/client/src/components/navigation/Navbar.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 export const Navbar = () => {
      const navigate = useNavigate();
     return (
-        <HStack justifyContent={"flex-start"} bgColor={"lightgrey"} height={"18%"} width={"100%"} padding={5}>
+        <HStack justifyContent={"flex-start"} bgColor={"lightgrey"} height={"18%"} width={"100%"} border="1px" borderColor="gray.300" boxShadow="md" padding={5}>
             <Button bgImage={"main.png"} bgPosition={"center"} bgRepeat={"no-repeat"} bgSize={"100%"} height={"100%"} width={"15%"} onClick={() => navigate("/")} variant="link"></Button>
         </HStack>
     )
