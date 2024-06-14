68c7562d6de5d77b006dc05191feef949cf3f06c
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-06-14 21:16:26 +0700

    Added main logo on Navbar

--- projects/client/src/components/navigation/Navbar.js
+++ projects/client/src/components/navigation/Navbar.js
@@ -1,9 +1,10 @@
-import { HStack, Text } from '@chakra-ui/react'
+import { Box, HStack, Text } from '@chakra-ui/react'
 import React from 'react'
 
 export const Navbar = () => {
   return (
-    <HStack justifyContent={"flex-start"} bgColor="grey" padding={5} width={"100%"}>
+    <HStack justifyContent={"flex-start"} bgColor={"lightgrey"} height={"18%"} width={"100%"} padding={5}>
+        <Box bgImage={"main.png"} bgPosition={"center"} bgRepeat={"no-repeat"} bgSize={"100%"} height={"100%"} width={"15%"} ></Box>
         <Text>Navigation Bar</Text>
     </HStack>
   )
