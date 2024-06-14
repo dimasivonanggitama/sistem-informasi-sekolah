2ff8b32d5ebde7bfefc60b6d477527e6f723d35b
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-06-14 21:43:22 +0700

    Added 'type' props paramater for 'guest' case

--- projects/client/src/components/navigation/Navbar.js
+++ projects/client/src/components/navigation/Navbar.js
@@ -2,11 +2,14 @@ import { Button, HStack } from '@chakra-ui/react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
-export const Navbar = () => {
+export const Navbar = (props) => {
      const navigate = useNavigate();
     return (
         <HStack justifyContent={"flex-start"} bgColor={"lightgrey"} height={"18%"} width={"100%"} border="1px" borderColor="gray.300" boxShadow="md" padding={5}>
             <Button bgImage={"main.png"} bgPosition={"center"} bgRepeat={"no-repeat"} bgSize={"100%"} height={"100%"} width={"15%"} onClick={() => navigate("/")} variant="link"></Button>
+            { props.type === "guest" ?
+                <Button colorScheme='blue' onClick={() => navigate("login")}>Login</Button>
+            : <></> }
         </HStack>
     )
 }
