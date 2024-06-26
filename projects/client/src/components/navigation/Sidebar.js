c4a8881ef34b6a82593db6af3ce90260b2a49e32
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-06-26 20:13:31 +0700

    Added accordion style on every section

--- projects/client/src/components/navigation/Sidebar.js
+++ projects/client/src/components/navigation/Sidebar.js
@@ -1,15 +1,30 @@
-import { Button, Text, VStack } from '@chakra-ui/react'
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Button, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 export const Sidebar = (props) => {
   return (
     <VStack alignItems={"flex-start"} bgColor={"lightgrey"} border="1px" borderColor="gray.300" borderTopColor={"gray"} borderTopStyle={"dashed"} boxShadow="md" height={"100%"} width={"15%"}>
+      <Accordion width={"100%"} defaultIndex={[0]} allowMultiple>
+        <AccordionItem>
+          <h2>
+            <AccordionButton bgColor={"orange.200"} _hover={{background: "orange.300", color: "black"}} >
+              <Box as='span' flex='1' textAlign='left'>
+                <Text fontStyle={"u"}>Manajemen Data</Text>
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            <VStack alignItems={"flex-start"} gap={0} width={"100%"}>
+              <Button justifyContent={"flex-start"} bgColor={"orange.200"} border={"1px"} borderColor={"gray"} _hover={{background: "orange.300", color: "black"}} variant={"solid"} width={"100%"}><Text paddingLeft={"10%"}>Data Siswa</Text></Button>
+              <Button justifyContent={"flex-start"} _hover={{background: "orange.300", color: "black"}} variant={"ghost"} width={"100%"}><Text paddingLeft={"10%"}>Data Orang Tua</Text></Button>
+              <Button justifyContent={"flex-start"} _hover={{background: "orange.300", color: "black"}} variant={"ghost"} width={"100%"}><Text paddingLeft={"10%"}>Data Karyawan</Text></Button>
+              <Button justifyContent={"flex-start"} _hover={{background: "orange.300", color: "black"}} variant={"ghost"} width={"100%"}><Text paddingLeft={"10%"}>Data Prestasi</Text></Button>
+            </VStack>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
       <Button justifyContent={"flex-start"} _hover={{background: "black", color: "white"}} variant={"ghost"} width={"100%"}><Text fontStyle={"italic"}>Manajemen Data</Text></Button>
-      <VStack alignItems={"flex-start"} gap={0} width={"100%"}>
-        <Button justifyContent={"flex-start"} bgColor={"orange.200"} border={"1px"} borderColor={"gray"} _hover={{background: "orange.200", color: "black"}} variant={"solid"} width={"100%"}><Text paddingLeft={"10%"}>Data Siswa</Text></Button>
-        <Button justifyContent={"flex-start"} _hover={{background: "black", color: "white"}} variant={"ghost"} width={"100%"}><Text paddingLeft={"10%"}>Data Orang Tua</Text></Button>
-        <Button justifyContent={"flex-start"} _hover={{background: "black", color: "white"}} variant={"ghost"} width={"100%"}><Text paddingLeft={"10%"}>Data Karyawan</Text></Button>
-      </VStack>
     </VStack>
   )
 }
