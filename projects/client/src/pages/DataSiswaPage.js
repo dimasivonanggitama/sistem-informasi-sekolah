a99809f051739f399860fcf19ed66bed007b8bc2
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-06-27 00:44:53 +0700

    Added navbar, sidebar, and blank page components

--- projects/client/src/pages/DataSiswaPage.js
+++ projects/client/src/pages/DataSiswaPage.js
@@ -1,7 +1,17 @@
+import { Flex, HStack, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { Navbar } from '../components/navigation/Navbar'
+import { Sidebar } from '../components/navigation/Sidebar'
 
 export const DataSiswaPage = () => {
   return (
-    <div>DataSiswaPage</div>
+    <Flex minH={"100vh"} bg={"orange.100"}>
+      <VStack gap={0} width={"100%"}>
+        <Navbar type="guest"/>
+        <HStack height={"100%"} width={"100%"}>
+          <Sidebar/>
+        </HStack>
+      </VStack>
+    </Flex>
   )
 }
