d94a94ba0de84ed24727b5496b275418c24bb860
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-06-28 20:33:30 +0700

    Added AppPageFrame custom component + currentPage property

--- projects/client/src/pages/ParentDataPage.js
+++ projects/client/src/pages/ParentDataPage.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { AppPageFrame } from './AppPageFrame'
 
 export const ParentDataPage = () => {
+  var currentPage = "dataorangtua";
   return (
-    <div>ParentDataPage</div>
+    <AppPageFrame currentPage={currentPage} role="employee">
+
+    </AppPageFrame>
   )
 }
