d69e83ada97e24d9026bc47098c87dc215d12c70
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-06-28 20:54:36 +0700

    Added login & datasiswa route pages/elements

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/LandingPage";
 // import { ForgotPasswordPage } from "./pages/ForgotPasswordPage";
 import { LoginPage } from "./pages/LoginPage";
+import { DataSiswaPage } from "./pages/DataSiswaPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage/>} />
         <Route path="/login" element={<LoginPage/>} />
+        <Route path="/datasiswa" element={<DataSiswaPage/>} />
         {/* <Route path="/forgot-password" element={<ForgotPasswordPage/>} /> */}
       </Routes>
     </Router>
