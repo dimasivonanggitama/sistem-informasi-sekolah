77c8ed6d0879d4f7c33a5d6d72c813fc64879824
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-07-03 22:33:48 +0700

    Added AttendanceButton component + more data table

--- projects/client/src/pages/StudentAttendancePage.js
+++ projects/client/src/pages/StudentAttendancePage.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { AppPageFrame } from './AppPageFrame'
 import { IconButton, Table, TableCaption, TableContainer, Tbody, Td, Text, Tfoot, Th, Thead, Tr } from '@chakra-ui/react'
-import { TbCheck, TbWashDry } from 'react-icons/tb'
+import { TbCheck, TbFirstAidKit, TbInfoCircle, TbWashDry, TbX } from 'react-icons/tb'
+import { AttendanceButton } from '../components/button/AttendanceButton'
 
 export const StudentAttendancePage = () => {
     return (
@@ -14,13 +15,13 @@ export const StudentAttendancePage = () => {
                 <Thead>
                     <Tr>
                         <Th>Nama Siswa</Th>
-                        <Th>25 Juni 2024</Th>
-                        <Th>26 Juni 2024</Th>
-                        <Th>27 Juni 2024</Th>
+                        <Th>30 Juni 2024</Th>
                         <Th>01 Juli 2024</Th>
                         <Th>02 Juli 2024</Th>
                         <Th>03 Juli 2024</Th>
                         <Th>04 Juli 2024</Th>
+                        <Th>05 Juli 2024</Th>
+                        <Th>08 Juli 2024</Th>
                     </Tr>
                 </Thead>
                 <Tbody>
@@ -30,19 +31,99 @@ export const StudentAttendancePage = () => {
                         <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
                         <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
                         <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
-                        <Td><IconButton isRound={true} variant='solid' colorScheme='gray' aria-label='Unchecked' fontSize='20px' icon={<TbWashDry />}/></Td>
-                        <Td><IconButton isRound={true} variant='solid' colorScheme='gray' aria-label='Unchecked' fontSize='20px' icon={<TbWashDry />}/></Td>
-                        <Td><IconButton isRound={true} variant='solid' border={"1px"} borderColor={"gray"} colorScheme='gray' aria-label='Unchecked' fontSize='20px' icon={<TbWashDry />}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
                     </Tr>
                     <Tr>
                         <Td>Chairani Kalla</Td>
                         <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='blue' aria-label='Present' fontSize='20px' icon={<TbInfoCircle />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                    </Tr>
+                    <Tr>
+                        <Td>Gusti Raden Ajeng Nurastuti Wijareni</Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='blue' aria-label='Present' fontSize='20px' icon={<TbInfoCircle />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='red' aria-label='Present' fontSize='20px' icon={<TbX />}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                    </Tr>
+                    <Tr>
+                        <Td>Izzat Assegaf</Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='orange' aria-label='Present' fontSize='20px' icon={<TbFirstAidKit />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                    </Tr>
+                    <Tr>
+                        <Td>Kaesang Pangarep</Td>
                         <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
                         <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
                         <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
-                        <Td><IconButton isRound={true} variant='solid' colorScheme='gray' aria-label='Unchecked' fontSize='20px' icon={<TbWashDry />}/></Td>
-                        <Td><IconButton isRound={true} variant='solid' colorScheme='gray' aria-label='Unchecked' fontSize='20px' icon={<TbWashDry />}/></Td>
-                        <Td><IconButton isRound={true} variant='solid' border={"1px"} borderColor={"gray"} colorScheme='gray' aria-label='Unchecked' fontSize='20px' icon={<TbWashDry />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                    </Tr>
+                    <Tr>
+                        <Td>Kevin Apriliano Sumaatmadja</Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='red' aria-label='Present' fontSize='20px' icon={<TbX />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='blue' aria-label='Present' fontSize='20px' icon={<TbInfoCircle />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                    </Tr>
+                    <Tr>
+                        <Td>Muhammad Alif Fakhri</Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><AttendanceButton /></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                    </Tr>
+                    <Tr>
+                        <Td>Muhammad Zikri Bintani</Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' border={"1px"} borderColor={"gray"} colorScheme='gray' aria-label='Present' fontSize='20px' icon={<TbWashDry />}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                    </Tr>
+                    <Tr>
+                        <Td>Muhammad Zinedine Alam Ganjar</Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' border={"1px"} borderColor={"gray"} colorScheme='gray' aria-label='Present' fontSize='20px' icon={<TbWashDry />}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                    </Tr>
+                    <Tr>
+                        <Td>Putri Indahsari Tanjung Jatikusumo</Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Present' fontSize='20px' icon={<TbCheck />}/></Td>
+                        <Td><IconButton isRound={true} variant='solid' border={"1px"} borderColor={"gray"} colorScheme='gray' aria-label='Present' fontSize='20px' icon={<TbWashDry />}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
+                        <Td><AttendanceButton isDisabled={true}/></Td>
                     </Tr>
                 </Tbody>
                     <Tfoot>
