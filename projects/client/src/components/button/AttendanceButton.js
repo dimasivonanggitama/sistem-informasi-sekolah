668d49f8891317f6773e9a61dc8fdfad7ee7ffd0
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-07-03 20:40:26 +0700

    Added custom attendance menu button

--- projects/client/src/components/button/AttendanceButton.js
+++ projects/client/src/components/button/AttendanceButton.js
@@ -1,7 +1,48 @@
+import { Button, HStack, IconButton, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react'
 import React from 'react'
+import { TbCheck, TbCircleFilled, TbFirstAidKit, TbInfoCircle, TbWashDry, TbX } from 'react-icons/tb'
 
-export const AttendanceButton = () => {
+export const AttendanceButton = (props) => {
   return (
-    <div>AttendanceButton</div>
+    <Menu>
+        <MenuButton 
+            as={IconButton} 
+            border="1px"
+            borderColor="gray"
+            isRound={true}
+            variant='solid'
+            colorScheme='gray'
+            aria-label='Done'
+            fontSize='20px'
+            icon={<TbWashDry/>} 
+            isDisabled={props.isDisabled}
+        ></MenuButton>
+        <MenuList>
+            <MenuItem as={Button} justifyContent={"flex-start"} color={"green"} leftIcon={<TbCheck />}>
+              <HStack width="100%" justifyContent={"space-between"}>
+                <Text>Hadir</Text>
+                <TbCircleFilled />
+              </HStack>
+            </MenuItem>
+            <MenuItem as={Button} justifyContent={"flex-start"} color={"orange.500"} leftIcon={<TbFirstAidKit />}>
+              <HStack width="100%" justifyContent={"space-between"}>
+                <Text>Sakit</Text>
+                <TbCircleFilled />
+              </HStack>
+            </MenuItem>
+            <MenuItem as={Button} justifyContent={"flex-start"} color={"blue"} leftIcon={<TbInfoCircle />}>
+              <HStack width="100%" justifyContent={"space-between"}>
+                <Text>Izin</Text>
+                <TbCircleFilled />
+              </HStack>
+            </MenuItem>
+            <MenuItem as={Button} justifyContent={"flex-start"} color={"red"} leftIcon={<TbX />}>
+              <HStack width="100%" justifyContent={"space-between"}>
+                <Text>Alfa</Text>
+                <TbCircleFilled/>
+              </HStack>
+            </MenuItem>
+        </MenuList>
+    </Menu>
   )
 }
