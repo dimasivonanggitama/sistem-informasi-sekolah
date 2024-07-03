9f54868983eab06b0e146ea9e3199df9cfbecccf
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-07-03 20:49:20 +0700

    Added height and change color on main content area

--- projects/client/src/pages/AppPageFrame.js
+++ projects/client/src/pages/AppPageFrame.js
@@ -5,12 +5,12 @@ import { Sidebar } from '../components/navigation/Sidebar'
 
 export const AppPageFrame = (props) => {
   return (
-    <Flex minH={"100vh"} bg={(props.role === "employee") ? "orange.100" : "blue.100"}>
+    <Flex minH={"100vh"} bg={(props.role === "employee") ? "orange.100" : "#CEE3F0"}>
       <VStack gap={0} width={"100%"}>
         <Navbar type="guest"/>
         <HStack height={"100%"} width={"100%"}>
           <Sidebar currentPage={props.currentPage}/> {/* Sidebar has a width of 15% */}
-          <VStack alignItems={"flex-start"} padding={5} width={"85%"}>
+          <VStack alignItems={"flex-start"} height={"100%"} padding={5} width={"85%"}>
             {props.children}
           </VStack>
         </HStack>
