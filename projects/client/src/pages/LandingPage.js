43f4ca5fffe5129a1788eb929999ce151ef9d90a
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-07-26 04:51:31 +0700

    Arranged layout page

--- projects/client/src/pages/LandingPage.js
+++ projects/client/src/pages/LandingPage.js
@@ -1,4 +1,4 @@
-import { Button, Flex, HStack, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, Flex, HStack, Stack, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Navbar } from '../components/navigation/Navbar'
@@ -6,10 +6,17 @@ import { Navbar } from '../components/navigation/Navbar'
 const LandingPage = () => {
   const navigate = useNavigate();
   return (
-    <Flex minH={"100vh"} bg="pink">
+    <Flex bg={"pink"}>
       <VStack width={"100%"}>
         <Navbar type="guest"/>
-        <Text textColor={"white"}>This is LandingPage</Text>
+        <Stack height={"435px"} width={"100%"} bgColor={"red"} justifyContent={"center"} alignItems={"center"}><Text as="b" fontSize={"6xl"} color={"white"}>Carousel</Text></Stack>
+        <Stack height={"435px"} width={"100%"} bgColor={"blue"} justifyContent={"center"} alignItems={"center"}><Text as="b" fontSize={"6xl"} color={"white"}>Video</Text></Stack>
+        <Stack height={"400px"} width={"100%"} bgColor={"cyan"} justifyContent={"center"} alignItems={"center"}><Text as="b" fontSize={"6xl"} color={"white"}>Fasilitas</Text></Stack>
+        <Stack height={"400px"} width={"100%"} bgColor={"red"} justifyContent={"center"} alignItems={"center"}><Text as="b" fontSize={"6xl"} color={"white"}>Program</Text></Stack>
+        <Stack height={"400px"} width={"100%"} bgColor={"purple"} justifyContent={"center"} alignItems={"center"}><Text as="b" fontSize={"6xl"} color={"white"}>Instagram Highlights</Text></Stack>
+        <Stack height={"400px"} width={"100%"} bgColor={"grey"} justifyContent={"center"} alignItems={"center"}><Text as="b" fontSize={"6xl"} color={"white"}>Galeri</Text></Stack>
+        <Stack height={"400px"} width={"100%"} bgColor={"green"} justifyContent={"center"} alignItems={"center"}><Text as="b" fontSize={"6xl"} color={"white"}>What people say</Text></Stack>
+        <Stack height={"400px"} width={"100%"} bgColor={"orange"} justifyContent={"center"} alignItems={"center"}><Text as="b" fontSize={"6xl"} color={"white"}>Footer</Text></Stack>
       </VStack>
     </Flex>
   )
