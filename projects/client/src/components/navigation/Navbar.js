e3f550f14430ffd9fddb4bda7bdfe718a28c3eb5
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2024-07-26 21:04:55 +0700

    Rearranged navbar layout

--- projects/client/src/components/navigation/Navbar.js
+++ projects/client/src/components/navigation/Navbar.js
@@ -5,11 +5,13 @@ import { useNavigate } from 'react-router-dom';
 export const Navbar = (props) => {
     const navigate = useNavigate();
     return (
-        <HStack justifyContent={"flex-start"} bgColor={"lightgrey"} height={"15%"} width={"100%"} border="1px" borderColor="gray.300" boxShadow="md" paddingX={5}>
-            <Button bgImage={"main.png"} bgPosition={"center"} bgRepeat={"no-repeat"} bgSize={"100%"} height={"100%"} width={"15%"} onClick={() => navigate("/")} variant="link"></Button>
-            { props.type === "guest" ?
-                <Button colorScheme='blue' onClick={() => navigate("login")}>Login</Button>
-            : <></> }
+        <HStack justifyContent={"center"} bgColor={"lightgrey"} border={"1px"} borderColor={"gray.300"} boxShadow={"md"} height={"100px"} width={"100%"}>
+            <HStack justifyContent={"space-between"} width={"1500px"} height={"100%"} bgColor={"darkgrey"}>
+                <Button bgImage={"main.png"} bgRepeat={"no-repeat"} bgSize={"contain"} height={"100%"} width={"320px"} onClick={() => navigate("/")} variant="link"></Button>
+                { props.type === "guest" ?
+                    <Button colorScheme='blue' onClick={() => navigate("login")}>Login</Button>
+                : <></> }
+            </HStack>
         </HStack>
     )
 }
